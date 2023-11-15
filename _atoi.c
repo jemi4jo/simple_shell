9047d1b6f1f453b8baa99b251d385a53d9cfc74a
jemi4jo <jemi4jo@gmail.com>
2023-11-15 15:15:49 +0300

    fix error 7

--- _atoi.c
+++ _atoi.c
@@ -63,15 +63,3 @@ int _atoi(char *s)
 	return (output);
 }
 
-void bfree(void **buffer)
-{
-    if (buffer)
-    {
-        int i;
-        for (i = 0; buffer[i]; ++i)
-        {
-            free(buffer[i]); /* Free each element in the buffer */
-        }
-        free(buffer); /* Free the buffer itself */
-    }
-}
